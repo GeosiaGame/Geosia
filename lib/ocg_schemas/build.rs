6f49d4b5a688d2fe5454bc0335a713dd40cb7b5d
Raven Szewczyk <git@eigenraven.me>
2023-08-04 22:47:31 +0100

    Require explicit enable for capnp schemas

--- lib/ocg_schemas/build.rs
+++ lib/ocg_schemas/build.rs
@@ -1,11 +1,16 @@
-
-use capnpc::CompilerCommand as Capnp;
-
 fn main() {
-    Capnp::new()
-        .src_prefix("capnp/")
-        .import_path("capnp/")
-        .file("capnp/game_types.capnp")
-        .run()
-        .expect("compiling capnp schema");
+    #[cfg(feature = "regenerate-capnp")]
+    {
+        use capnpc::CompilerCommand as Capnp;
+
+        std::fs::remove_dir_all("capnp-generated/");
+
+        Capnp::new()
+            .src_prefix("capnp/")
+            .import_path("capnp/")
+            .output_path("capnp-generated/")
+            .file("capnp/game_types.capnp")
+            .run()
+            .expect("compiling capnp schema");
+    }
 }
--- lib/ocg_schemas/src/schemas/mod.rs
+++ lib/ocg_schemas/src/schemas/mod.rs
@@ -1,8 +1,11 @@
 //! Generic code for a stable forwards/backwards compatible serialization framework for game objects.
 //!
 //! Overview:
-//!  - Based on 
+//!  - Based on
 
 pub mod game_types_capnp {
-    include!(concat!(env!("OUT_DIR"), "/game_types_capnp.rs"));
+    include!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/capnp-generated/game_types_capnp.rs"
+    ));
 }
