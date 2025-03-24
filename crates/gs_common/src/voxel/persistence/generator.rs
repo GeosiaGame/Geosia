c4c985d63fbc713e7912d866e3c4102c31bce5b2
Raven Szewczyk <git@eigenraven.me>
2025-03-24 17:43:55 +0000

    Edition 2024

--- crates/gs_common/src/voxel/persistence/generator.rs
+++ crates/gs_common/src/voxel/persistence/generator.rs
@@ -46,11 +46,11 @@ impl<ExtraData: GsExtraData> ChunkPersistenceLayer<ExtraData> for GeneratorPersi
             self.wip_task_counter.fetch_add(1, AtomicOrdering::Relaxed);
             let counter = Arc::clone(&self.wip_task_counter);
             let counter = CounterDecrOnDrop(counter);
-            let gen = Arc::clone(&self.generator);
+            let generator = Arc::clone(&self.generator);
             let extra_data = self.extra_data.clone();
             let task = AsyncComputeTaskPool::get().spawn(async move {
                 let _counter = counter; // decrement on drop()
-                let chunk = gen.generate_chunk(pos, extra_data);
+                let chunk = generator.generate_chunk(pos, extra_data);
                 (pos, Ok(MutWatcher::new(chunk)))
             });
             let _ = self.live_tasks.try_insert(pos, task);
